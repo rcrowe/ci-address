1d4cc64a347500e3736f428f5a7118df4e94fd74
Rob Crowe <nobby.crowe@gmail.com>
2011-04-02 18:41:47 +0100

    Testing

--- Address.php
+++ Address.php
@@ -1,16 +1,15 @@
-<?php
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Address {
 	
+	protected $using_curl = true;
+	
 	public function __construct()
 	{
-		
-	}
-	
-	public function test()
-	{
-		echo 'Address';
+		$this->using_curl = (extension_loaded('curl') && file_exists(APPPATH.'libraries/Curl.php')) ? true : false;
 	}
 }
+// END Address Class
 
-?>
+/* End of file Address.php */
+/* Location: ./application/libraries/Address.php */
