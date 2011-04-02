81e8ae5a69bd52a3738de3d3a8e5dddc531d73a1
Rob Crowe <nobby.crowe@gmail.com>
2011-04-02 18:51:23 +0100

    Start of testing for Curl library

--- Address.php
+++ Address.php
@@ -8,18 +8,14 @@ class Address {
 	
 	public function __construct()
 	{
-		//$this->using_curl = (extension_loaded('curl') && file_exists(APPPATH.'libraries/Curl.php')) ? true : false;
-		
-		//Using Curl library
-		//$this->load
-		
 		$this->_ci =& get_instance();
 		
 		if(extension_loaded('curl') && file_exists(APPPATH.'libraries/Curl.php'))
 		{
 			$this->_ci->load->library('curl');
 			
-			if(method_exists($this->_ci->curl, 'simple_get'))
+			//Using _simple_call not simple_get, as Curl uses __call
+			if(method_exists($this->_ci->curl, '_simple_call'))
 			{
 				$this->using_curl = true;
 			}
